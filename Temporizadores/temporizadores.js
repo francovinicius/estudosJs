0fb577920b561120af8e20cdb862bf02b4b419b5
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-27 13:53:15 -0300

    temporizadores

---
+++ Temporizadores/temporizadores.js
@@ -0,0 +1,20 @@
+// setInterval - executa de tempos em tempos infinitamente
+
+function acao () {
+    document.write("Executando... </br>")
+  
+}
+
+/*
+1000 ms = 1s
+setInterval(acao, 1000)
+
+var timer = setInterval(acao, 1000)
+clearInterval(timer) -> parando o intervalo
+*/
+
+
+
+//setTimeout
+
+setTimeout (acao, 1000); // daqui 1s execute a função setada
