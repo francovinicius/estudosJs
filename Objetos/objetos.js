0d74ea294c6b7e4af38e8bce08dae62c81990da0
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-27 15:24:01 -0300

    objetos

---
+++ Objetos/objetos.js
@@ -0,0 +1,39 @@
+let lista = [123, 4, 'vini'];
+
+//objeto
+
+let pessoa = {
+    nome: 'Vini',
+    idade: 29,
+    altura: 1.81,
+    cargo: 'programador'
+}
+
+//console.log(pessoa.nome)
+
+let carro = {
+    nome: 'Gol',
+    cor: 'preto',
+    potencia: '140cv'
+
+}
+
+//console.log(carro)
+
+let usuarios = [
+    {
+        nome:'vini', cargo: 'programador', status: 'ativo'
+    },
+    {
+        nome:'pablo', cargo:'backend', status:'ativo'
+    },
+    {
+        nome:'isa', cargo:'rh', status:'ativo'
+    },
+    {
+        nome:'cleo', cargo:'rh', status:'inativo'
+    }
+]
+
+console.log(usuarios)
+console.log(usuarios[1].cargo)
