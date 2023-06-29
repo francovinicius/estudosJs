15c88657dbeb5aaad1057303edf2670678e0dcab
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-29 16:10:28 -0300

    add components and props

--- ReactJs/primeiro-app/src/App.js
+++ ReactJs/primeiro-app/src/App.js
@@ -1,9 +1,15 @@
+import Nome from "./Components/Nome";
+
 
 
 function App() {
   return (
     <div className="">
-      bem vindo
+      <h1>Hope</h1>
+
+      <Nome aluno='vinicius' idade={29} />
+      <br />
+      <Nome aluno='pablo' idade={23} />
     </div>
   );
 }
---
+++ ReactJs/primeiro-app/src/Components/Nome/index.js
@@ -0,0 +1,6 @@
+//Props
+
+export default function Nome ({aluno, idade}) {
+    return(
+    <span>Bem Vindo Aluno: {aluno} - Idade: {idade} </span>
+);}
