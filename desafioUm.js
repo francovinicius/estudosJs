ed198159448f0a6d480a93565efc43b42eec5f8d
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-29 10:48:35 -0300

    add h1

--- desafioUm.js
+++ desafioUm.js
@@ -25,7 +25,7 @@ function calcular(event) {
     </div>
     </div>
     </br>
-    <a href="/" class="btn btn-primary d-flex justify-content-center">Novo Teste</a>
+    <a href="/" class="btn btn-primary d-flex justify-content-center">Novo Teste!</a>
 `);
 
     } else if (imc > 17 && imc <= 18.49) {
@@ -38,7 +38,7 @@ function calcular(event) {
     </div>
     </div>
     </br>
-    <a href="/" class="btn btn-primary d-flex justify-content-center">Novo Teste</a>`);
+    <a href="/" class="btn btn-primary d-flex justify-content-center">Novo Teste!</a>`);
 
     } else if (imc > 18.49 && imc <= 24.99) {
         resultado.innerHTML = (`</br>
@@ -50,7 +50,7 @@ function calcular(event) {
     </div>
     </div>
     </br>
-    <a href="/" class="btn btn-primary d-flex justify-content-center">Novo Teste</a>`);
+    <a href="/" class="btn btn-primary d-flex justify-content-center">Novo Teste!</a>`);
 
     } else if (imc > 24.99) {
 
@@ -63,7 +63,7 @@ function calcular(event) {
     </div>
     </div>
     </br>
-    <a href="/" class="btn btn-primary d-flex justify-content-center">Novo Teste</a>`);
+    <a href="/" class="btn btn-primary d-flex justify-content-center">Novo Teste!</a>`);
     }
 
 
